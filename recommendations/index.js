f85367bc9d5609fd2f6697c7cac8cdf792932b1a
Christopher Cross <cc28@alumni.princeton.edu>
2020-09-17 01:38:56 -0700

    use crawler npm module, remove apify SDK, add crawler to recommender post endpoint

--- recommendations/index.js
+++ recommendations/index.js
@@ -3,13 +3,31 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const KeyWordExtractor = require('./src/keywordExtractor'); 
 
+
+// @ts-ignore
+const Crawler = require("crawler");
+
 const app = express(); 
 
 app.use(bodyParser.json());
 app.use(cors()); 
 
 
-
+let c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        done();
+    }
+});
 
 app.post('/recommendations', (req, res) => {
 
@@ -19,6 +37,8 @@ app.post('/recommendations', (req, res) => {
 
     // Next, clean the user input to remove stopwords and 
     // punctuation 
+
+    c.queue(['https://plato.stanford.edu/entries/time/', 'https://plato.stanford.edu/entries/time-thermo/']);
 
     const keywords = KeyWordExtractor.getKeywords(TextInput);
 
