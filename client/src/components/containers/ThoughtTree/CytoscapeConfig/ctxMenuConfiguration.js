3e9c94619ffb4322c15070bca5d62be894cbc4a0
Christopher Cross <cc28@alumni.princeton.edu>
2020-09-16 23:41:41 -0700

    import ctxMenu config options, remove configureCTXMenu method

---
+++ client/src/components/containers/ThoughtTree/CytoscapeConfig/ctxMenuConfiguration.js
@@ -0,0 +1,26 @@
+
+
+/**
+ * This object is reponsible for initializing the ctxMenu. The commands
+ * must be bound at a later point. 
+ */
+const ctxMenuConfiguration = {
+    menuRadius: 100, // the radius of the circular menu in pixels
+    selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
+    commands: null, // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
+    fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
+    activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
+    activePadding: 20, // additional size in pixels for the active command
+    indicatorSize: 24, // the size in pixels of the pointer to the active command
+    separatorWidth: 3, // the empty spacing in pixels between successive commands
+    spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
+    minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight
+    maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight
+    openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
+    itemColor: 'white', // the colour of text in the command's content
+    itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
+    zIndex: 9999, // the z-index of the ui div
+    atMouse: false // draw menu at mouse position
+};
+
+export default ctxMenuConfiguration; 
--- client/src/components/containers/ThoughtTree/Diagram.js
+++ client/src/components/containers/ThoughtTree/Diagram.js
@@ -8,6 +8,7 @@ import cytoscapeStylesArray from './CytoscapeStyles'
 import Panel from "../../UI/Panel/Panel";
 import { v4 as uuidv4 } from 'uuid';
 import { connect } from 'react-redux';
+import ctxMenuConfigObject from "./CytoscapeConfig/ctxMenuConfiguration"; 
 
 cytoscape.use(cxtmenu);
 
@@ -39,35 +40,14 @@ class Diagram extends Component {
 
     myCyRef = React.createRef();
 
-    configureCTXMenu = (Commands) => {
-        const ctxMenuConfiguration = {
-            menuRadius: 100, // the radius of the circular menu in pixels
-            selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
-            commands: Commands, // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
-            fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
-            activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
-            activePadding: 20, // additional size in pixels for the active command
-            indicatorSize: 24, // the size in pixels of the pointer to the active command
-            separatorWidth: 3, // the empty spacing in pixels between successive commands
-            spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
-            minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight
-            maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight
-            openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
-            itemColor: 'white', // the colour of text in the command's content
-            itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
-            zIndex: 9999, // the z-index of the ui div
-            atMouse: false // draw menu at mouse position
-        };
-
-        this.setState({ ctxMenuConfiguration: ctxMenuConfiguration });
-    }
-
-
-
     componentDidMount() {
 
         const commands = [this.DesignateAsThesis, this.printToConsole, this.OpenInPanel, this.AddSupportNode, this.AddOpposeNode];
-        this.configureCTXMenu(commands);
+
+        const ctxMenuConfigObjectWithCmds = { ...ctxMenuConfigObject, commands : commands }; 
+
+        this.setState({ ctxMenuConfiguration: ctxMenuConfigObjectWithCmds });
+
 
         this.setState({ cyCoreListeners: this.myCyRef._private.emitter.listeners });
 
