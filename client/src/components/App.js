0965bddf8a2d2a0d6f96b528f5e12ceb07715bf3
Christopher Cross <cc28@alumni.princeton.edu>
2020-09-19 20:33:47 -0700

    remove unused state and methods in app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,83 +12,12 @@ import SignUp from "../containers/Authentication/SignUp/SignUp";
 
 export default class App extends Component {
 
-    state = {
-
-        nodeDataArray: [
-            { data: { id: 'one', label: `Node 1`, text: "Immutable benefits performance here by reducing the total number of diff() calls needed. For example, an unchanged element requires only one diff with Immutable whereas it would require many diffs with the default JSON diff() implementation. Basically, Immutable make diffs minimal-depth searche" }},
-            { data: { id: 'two', label: `Node 2`, text: "I hope this post will be an important milestone in your journey into Functional Programming, as well as a source of information to go back to when needed." }}
-        ],
-
-        linkDataArray: [
-            { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-        ],
-
-        newNodeContent: ''
-
-    }
-
     componentDidUpdate() {
-
-        // Either the user has submitted an empty draft or
-        // create new node has already been called and this check
-        // prevents the indefinite loop 
-        if (!this.state.newNodeContent) { return; }
-
-        this.createNewNode(this.props.newNodeContent);
 
         console.log('[Diagram.js]: componentDidUpdate');
 
     }
 
-
-    createNewNode = (newNodeContent) => {
-
-        let newNodeDataArray = [...this.state.nodeDataArray];
-
-        const newNode = {
-            data: {
-                id: 'three',
-                label: 'Node 3',
-                text: newNodeContent,
-            },
-            position: { x: 400, y: 400 }
-        }
-
-        let newLinkDataArray = [...this.state.linkDataArray];
-
-        const newLink = {
-            data: {
-                source: 'one',
-                target: newNode.data.id,
-                label: 'new label for new edge'
-            }
-        }
-
-        newNodeDataArray.push(newNode);
-
-        newLinkDataArray.push(newLink);
-
-
-        this.setState({
-            nodeDataArray: newNodeDataArray,
-            linkDataArray: newLinkDataArray,
-
-            // Very important to update this value to null to prevent 
-            // an indefinite loop. 
-            newNodeContent: null,
-        });
-
-
-        console.log('new node created');
-
-
-    }
-
-
-
-    liftNewNodeContent = (newContent) => {
-        this.setState({ newNodeContent: newContent });
-    }
 
 
 
@@ -103,15 +32,7 @@ export default class App extends Component {
                         <Route path='/login' exact component={SignUp} />
                         <Route path='/test' exact render={() => (<h1>Test Routing Page</h1>)} />
                         <Route path='/about' exact component={AboutBlurb} />
-                        <Route path='/thoughtTree' exact render={() => {
-                            return (
-                                <Diagram
-                                    nodeDataArray={this.state.nodeDataArray}
-                                    linkDataArray={this.state.linkDataArray}
-
-                                />
-                            )
-                        }} />
+                        <Route path='/thoughtTree' exact component={Diagram} />
                     </Switch>
                 </Layout>
             </BrowserRouter>
